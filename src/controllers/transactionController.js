daf32e62fd99373a6b6d96bc8bdb733d222bc68b
Matheus <matheusvmja@gmail.com>
2023-07-09 22:46:38 -0300

    Get/home completed

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -4,9 +4,19 @@ export async function newTransaction(req, res) {
     const { value, description, type } = req.body
 
     try {
-        const transaction = { value: Number(value), description, type}
-            await db.collection("transactions").insertOne("transaction")
+        const transaction = { value: Number(value), description, type, time: Date.now() }
+        await db.collection("transactions").insertOne("transaction")
         res.sendStatus(201)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+}
+
+export async function showTransactions(req, res) {
+
+    try {
+        const transactions = await db.collection("transactions").find().sort({ time: -1 }).toArray()
+        res.send(transactions)
     } catch (err) {
         res.status(500).send(err.message)
     }
--- src/routes/transactionRouter.js
+++ src/routes/transactionRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { newTransaction } from "../controllers/transactionController.js"
+import { newTransaction, showTransactions } from "../controllers/transactionController.js"
 import { validateAuth } from "../middlewares/validateAuth.js"
 import { validateSchema } from "../middlewares/validateSchema.js"
 import { transactionSchema } from "../schemas/transactionSchema.js"
@@ -7,6 +7,7 @@ import { transactionSchema } from "../schemas/transactionSchema.js"
 const transactionRouter = Router()
 transactionRouter.use(validateAuth)
 
-userRouter.post("/nova-transacao", validateSchema(transactionSchema), newTransaction)
+transactionRouter.post("/nova-transacao", validateSchema(transactionSchema), newTransaction)
+transactionRouter.get("/home", showTransactions)
 
 export default transactionRouter
